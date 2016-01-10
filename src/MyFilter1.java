cbc5c3852e56878a3fb6bf0e34966f7a82a5fdbd
kbhardwj <kaushalbhardwaj12@gmail.com>
2016-01-10 15:21:47 +0530

    initial commit

---
+++ src/MyFilter1.java
@@ -0,0 +1,55 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class MyFilter1
+ */
+//@WebFilter("/MyFilter1")
+public class MyFilter1 implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public MyFilter1() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+		response.setContentType("text/html");	
+		PrintWriter out=response.getWriter();
+		out.println("In filter1,filtering request......<br/>");
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+		out.println("In filter1,filtering response......<br/>");
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
